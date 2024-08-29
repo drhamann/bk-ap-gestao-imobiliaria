306379a0728a8e79fc8666417bc6b08ca6ec9402
George Hamann <george.hamann@gmail.com>
2024-08-29 20:29:54 -0300

    Conteudo base da aula, criando dados a partir do banco.

---
+++ src/Academia.Programador.Bk.Gestao.Imobiliaria/Academia.Programador.Bk.Gestao.Imobiliaria.Dominio/Model/Favorito.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Academia.Programador.Bk.Gestao.Imobiliaria.Web;
+
+public partial class Favorito
+{
+    public int FavoritoId { get; set; }
+
+    public int ClienteId { get; set; }
+
+    public int ImovelId { get; set; }
+
+    public DateOnly DataAdicionado { get; set; }
+
+    public virtual Cliente Cliente { get; set; } = null!;
+
+    public virtual Imovel Imovel { get; set; } = null!;
+}
